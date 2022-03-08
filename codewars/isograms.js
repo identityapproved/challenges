8020bbb6dac9c81969329e303e6c3f5b5a2206fd
identityapproved <identityapproved@gmail.com>
2022-03-08 23:33:20 +0200

    solve isograms

---
+++ codewars/isograms.js
@@ -0,0 +1,44 @@
+/* An isogram is a word that has no repeating letters, consecutive or non - consecutive.Implement a function that determines whether a string that contains only letters is an isogram.Assume the empty string is an isogram.Ignore letter case. */
+
+/* function isIsogram(str) {
+  const counts = {};
+
+  for (let i = 0; i < str.length; i++) {
+    const letters = str[i].toLowerCase();
+
+    if (counts[letters] || letters === "") {
+      return false;
+    }
+    counts[letters] = 1;
+  }
+  return true;
+} */
+
+/* function isIsogram(str) {
+  const counts = {};
+
+  return !str
+    .toLowerCase()
+    .split("")
+    .some((letters) => {
+      if (!counts[letters]) {
+        counts[letters] = 1;
+        console.log(counts);
+        return false;
+      } else {
+        return true;
+      }
+    });
+} */
+
+function isIsogram(str) {
+  const unique = new Set(str.toLowerCase());
+  return unique.size == str.length;
+}
+
+console.log(isIsogram("Dermatoglyphics")); // true);
+console.log(isIsogram("isogram")); // true);
+console.log(isIsogram("aba")); // false, "same chars may not be adjacent");
+console.log(isIsogram("moOse")); // false, "same chars may not be same case");
+console.log(isIsogram("isIsogram")); // false);
+console.log(isIsogram("")); // true, "an empty string is a valid isogram");
