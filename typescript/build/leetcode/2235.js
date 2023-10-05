15d8eef409fd73d4cb2020b942e69e8fbb44fe73
identityapproved <identityapproved@gmail.com>
2023-10-05 15:24:11 +0300

    Preparation 4 coding challenges

---
+++ typescript/build/leetcode/2235.js
@@ -0,0 +1,7 @@
+"use strict";
+function sum(num1, num2) {
+    return num1 + num2;
+}
+;
+console.log(sum(23, 23));
+//# sourceMappingURL=2235.js.map
---
+++ typescript/types/leetcode/2235.d.ts
@@ -0,0 +1,2 @@
+declare function sum(num1: number, num2: number): number;
+//# sourceMappingURL=2235.d.ts.map
